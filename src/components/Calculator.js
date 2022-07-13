a49c8cc7b4db884be1533cdbc67293035b2ab7c4
mirkan-k <mirkilinc@gmail.com>
2022-07-13 14:23:18 +0100

    Added Components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,12 +10,12 @@ export function Calculator() {
   const [val2, setVal2] = useState(initialVal);
   const [operator, setOperator] = useState('+');
   const [result, setResult] = useState(initialVal);
-  const [storedResult, setStoredResult] = useState(initialVal)
+  const [storedResult, setStoredResult] = useState(initialVal);
 
-  const addToValue1 = (e) => val1 === '0' ? setVal1(e.target.innerText) : setVal1(val1 + e.target.innerText)
-  const addToValue2 = (e) => val2 === '0' ? setVal2(e.target.innerText) : setVal2(val2 + e.target.innerText)
-  const addDecimalToValue1 = (e) => val1.includes('.') ? console.log("Can't add more decimal points!") : setVal1(val1 + e.target.innerText)
-  const addDecimalToValue2 = (e) => val2.includes('.') ? console.log("Can't add more decimal points!") : setVal2(val2 + e.target.innerText)
+  const addToValue1 = (e) => val1 === '0' ? setVal1(e.target.innerText) : setVal1(val1 + e.target.innerText);
+  const addToValue2 = (e) => val2 === '0' ? setVal2(e.target.innerText) : setVal2(val2 + e.target.innerText);
+  const addDecimalToValue1 = (e) => val1.includes('.') ? console.log("Can't add more decimal points!") : setVal1(val1 + e.target.innerText);
+  const addDecimalToValue2 = (e) => val2.includes('.') ? console.log("Can't add more decimal points!") : setVal2(val2 + e.target.innerText);
   
   const clearVal1 = () => setVal1(initialVal);
   const clearVal2 = () => setVal2(initialVal);
@@ -23,12 +23,12 @@ export function Calculator() {
     clearVal1();
     clearVal2();
     setResult(initialVal)
-  }
+  };
 
   const updateOperator = (e) => setOperator(e.target.innerText);
   const calcResult = () => setResult(Function(`"use strict";return ${val1} ${operator} ${val2}`))
-  const storeResult = () => setStoredResult(result)
-  const recallStoredResult = () => setResult(storedResult)
+  const storeResult = () => setStoredResult(result);
+  const recallStoredResult = () => setResult(storedResult);
 
   return(
     <div className="calculator">
@@ -40,5 +40,5 @@ export function Calculator() {
         
         <AnswerPad result={result} calcResult={calcResult} storeResult={storeResult} recallStoredResult={recallStoredResult} clearAll={clearAll} />
     </div>
-  )
+  );
 }
